15cd0463aa6df3247f12ec4ce32f4116a5dc7ef1
jaggannatha_r <chaithra.jagannatha_rao_telkar@mail.schwarz>
2025-06-29 19:51:16 +0200

    first push - Structure

---
+++ src/test/java/com/example/SCMS/demo/SCMS/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SCMS.demo.SCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
